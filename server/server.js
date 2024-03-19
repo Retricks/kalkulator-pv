319a421b5d30482eaa47af7f98e3ed6b31c1d915
Retricks <retrickspl@gmail.com>
2024-03-20 00:30:20 +0100

    9 commit

--- server/server.js
+++ server/server.js
@@ -8,13 +8,13 @@ const MySQLStore = require('express-mysql-session')(session);
 const { check, validationResult } = require('express-validator');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'sql98.lh.pl',
+  user: process.env.DB_USER || 'serwer198125',
+  password: process.env.DB_PASSWORD || 'Baz@D@n7ch',
+  database: process.env.DB_NAME || 'serwer198125_kalkulator',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -28,13 +28,13 @@ const sessionStore = new MySQLStore({
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use("/", (req, res) => {
   res.send("Server is running.")
 })
 
+
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'defaultSecretKey',
   resave: false,
   saveUninitialized: false,
   store: sessionStore,
