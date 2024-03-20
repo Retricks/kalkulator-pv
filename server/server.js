59a3da15395714755331f680559646243d37fad0
Retricks <retrickspl@gmail.com>
2024-03-20 11:38:42 +0100

    Magazyny bez modulow 2

--- server/server.js
+++ server/server.js
@@ -126,10 +126,8 @@ app.post('/api/saveResults', [
   validateInputs
 ], (req, res) => {
   const { imieKlienta, nazwiskoKlienta, moduly, liczbaModulow, falowniki, magazyny, konstrukcje, koordynacja, montaz, rodzajKlienta, narzut, mocPV, pojemnoscME, sumaNetto, sumaVat, sumaBrutto, cenaBazowa, sumaNettoKlienta, sumaVatKlienta, sumaBruttoKlienta, zarobek } = req.body;
-  console.log(req.body)
   const sql = 'INSERT INTO Oferty (ImieKlienta, NazwiskoKlienta, Moduly, LiczbaModulow, Falownik, Magazyn, Konstrukcja, Koordynacja, Montaz, RodzajKlienta, Narzut, MocPV, PojemnoscME, SumaNetto, SumaVAT, SumaBrutto, CenaBazowa, SumaNettoKlienta, SumaVatKlienta, SumaBruttoKlienta, Zarobek) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
   const values = [imieKlienta, nazwiskoKlienta, JSON.parse(moduly)?.Model || null, liczbaModulow || null, JSON.parse(falowniki).Model, JSON.parse(magazyny)?.Model || null, JSON.parse(konstrukcje)?.Rodzaj || null, JSON.parse(koordynacja).Firma, JSON.parse(montaz).Firma, rodzajKlienta, narzut, mocPV || null, pojemnoscME, sumaNetto, sumaVat, sumaBrutto, cenaBazowa, sumaNettoKlienta, sumaVatKlienta, sumaBruttoKlienta, zarobek];
-  console.log(values)
   pool.query(sql, values, (err, result) => {
     if (err) {
       console.error('Error saving results:', err);
