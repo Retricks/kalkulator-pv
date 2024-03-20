e6c1e83fc276d3016d62cd324e8eda87d9046b89
Retricks <retrickspl@gmail.com>
2024-03-20 10:50:00 +0100

    Magazyny bez modulow 2

--- server/server.js
+++ server/server.js
@@ -128,7 +128,7 @@ app.post('/api/saveResults', [
   const { imieKlienta, nazwiskoKlienta, moduly, liczbaModulow, falowniki, magazyny, konstrukcje, koordynacja, montaz, rodzajKlienta, narzut, mocPV, pojemnoscME, sumaNetto, sumaVat, sumaBrutto, cenaBazowa, sumaNettoKlienta, sumaVatKlienta, sumaBruttoKlienta, zarobek } = req.body;
 
   const sql = 'INSERT INTO Oferty (ImieKlienta, NazwiskoKlienta, Moduly, LiczbaModulow, Falownik, Magazyn, Konstrukcja, Koordynacja, Montaz, RodzajKlienta, Narzut, MocPV, PojemnoscME, SumaNetto, SumaVAT, SumaBrutto, CenaBazowa, SumaNettoKlienta, SumaVatKlienta, SumaBruttoKlienta, Zarobek) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-  const values = [imieKlienta, nazwiskoKlienta, JSON.parse(moduly)?.Model || null, liczbaModulow || null, JSON.parse(falowniki).Model, JSON.parse(magazyny)?.Model || null, JSON.parse(konstrukcje)?.Rodzaj || null, JSON.parse(koordynacja).Firma, JSON.parse(montaz).Firma, rodzajKlienta, narzut, mocPV, pojemnoscME, sumaNetto, sumaVat, sumaBrutto, cenaBazowa, sumaNettoKlienta, sumaVatKlienta, sumaBruttoKlienta, zarobek];
+  const values = [imieKlienta, nazwiskoKlienta, JSON.parse(moduly)?.Model || null, liczbaModulow || null, JSON.parse(falowniki).Model, JSON.parse(magazyny)?.Model || null, JSON.parse(konstrukcje)?.Rodzaj || null, JSON.parse(koordynacja).Firma, JSON.parse(montaz).Firma, rodzajKlienta, narzut, mocPV || null, pojemnoscME, sumaNetto, sumaVat, sumaBrutto, cenaBazowa, sumaNettoKlienta, sumaVatKlienta, sumaBruttoKlienta, zarobek];
 
   pool.query(sql, values, (err, result) => {
     if (err) {
